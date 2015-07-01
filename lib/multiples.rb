7174a6f8f2721b26b0fc0273068045b074f37638
AnnaErshova <anna.ershova@gmail.com>
2015-07-01 10:16:21 -0400

    even more refactoring

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -2,7 +2,7 @@ def collect_multiples(limit)
   sum = Array.new
   i = 3 # we know we need to start at 3
   while i < limit
-    sum << i if i%3 == 0 || i%5 == 0
+    sum << i if (i%3==0 || i%5==0)
     i += 1
   end
   sum
