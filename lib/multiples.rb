7b4943a9fe776d1769455bd87d75524e243490a0
AnnaErshova <anna.ershova@gmail.com>
2015-07-01 09:52:38 -0400

    all done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,40 @@
-# Enter your procedural solution here!
+
+=begin
+sum = 0
+i = 0
+
+while i < 1000
+  if i%3 == 0 || i%5 == 0
+    sum += i
+  end
+  i += 1
+end
+
+sum = []
+i = 0
+
+while i < 1000
+  if i%3 == 0 || i%5 == 0
+    sum << i
+  end
+  i += 1
+end
+sum.inject {|total, x| total + x }
+=end 
+
+def collect_multiples(limit)
+  sum = []
+  i = 1
+  while i < limit
+    if i%3 == 0 || i%5 == 0
+      sum << i
+     end
+    i += 1
+  end
+  sum
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
-# Enter your object-oriented solution here!
+class Multiples 
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    sum = []
+    i = 1
+    while i < @limit
+      if i%3 == 0 || i%5 == 0
+        sum << i
+      end
+      i += 1
+    end
+    sum
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+
+end
